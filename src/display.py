adaa13474d49f31eb7b43ffe66e98d64b8af1ac9
Anmol Rastogi <ai19btech11021@iith.ac.in>
2021-07-22 22:37:12 +0530

    Modified display.py to include grayscale image as well

--- src/display.py
+++ src/display.py
@@ -2,7 +2,7 @@ import torch
 import generator 
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage.color import rgb2lab,lab2rgb
+from skimage.color import rgb2lab,lab2rgb,rgb2gray
 def tensor_to_pic(tensor : torch.Tensor) -> np.ndarray:
     tensor[0] *=  100
     tensor[1:]*=  128
@@ -18,9 +18,11 @@ def show_images(n,dataset = images,gen=gen_model,dis=dis_model) -> None:
   image_tensor = dataset[n].to('cuda')
   gen_tensor = gen(image_tensor[0].unsqueeze(0).unsqueeze(0),z)[0]
   image = tensor_to_pic(image_tensor)
-  #print(torch.sum(gen_tensor))
+  tp = np.zeros_like(image)
+  gray_scale = rgb2gray(image)
+  tp[:,:,0] = tp[:,:,1] = tp[:,:,2] = gray_scale
   gen_image = tensor_to_pic(gen_tensor)
-  to_be_shown = np.concatenate((gen_image,image),axis=1)
+  to_be_shown = np.concatenate((gen_image,gray_scale,image),axis=1)
   plt.figure(figsize=(9,9))
   plt.imshow(to_be_shown)
   plt.show()
